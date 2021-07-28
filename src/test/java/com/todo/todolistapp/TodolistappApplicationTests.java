55bef235c318ffe11276b484319a4f65b130e7cf
ZhengyangWang <13679251160@163.com>
2021-07-28 15:27:31 +0800

    Init a springboot application

---
+++ src/test/java/com/todo/todolistapp/TodolistappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
