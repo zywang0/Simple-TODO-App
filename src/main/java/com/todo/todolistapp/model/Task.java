a8a73793371e8a297bf2067e346fc511e61213ad
zywang0 <13679251160@163.com>
2021-07-29 17:16:21 +0800

    [Zhengyang WANG] feat: implement task entity including title and description

--- src/main/java/com/todo/todolistapp/model/Task.java
+++ src/main/java/com/todo/todolistapp/model/Task.java
@@ -1,7 +1,30 @@
 package com.todo.todolistapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="task")
 public class Task {
-    private Long id;
-    private String title;
-    private boolean done;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "title", length = 50, nullable = false)
+    public String title;
+
+    @Column(name = "description", length = 200, nullable = false)
+    private String description;
+
+    public Task(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
